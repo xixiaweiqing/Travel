aeebf03333b5046bb8595590b0339ba44459e480
xmm <1195176384@qq.com>
2020-08-08 14:17:14 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
-
+import router from './router';
+import fastClick from 'fastclick'
+import './assets/styles/reset.css';
+import './assets/styles/border.css';
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+fastClick .attach(document.body);/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  components:{App},
+  template:'<App/>'
+
+  ,render: h => h(App) 
 })
