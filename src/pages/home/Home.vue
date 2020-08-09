e4d1856860d293e7c68293eb070f3979abbe8e61
xmm <1195176384@qq.com>
2020-08-09 13:29:46 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,13 +1,17 @@
 <template>
 <div>
-  Hello,world! <h1>啦啦啦啦啦</h1>
+  <home-header></home-header>
 </div>
  
 </template>
 
 <script>
+import HomeHeader from'./components/Header'
 export default{
-    name:'Home'
+    name:'Home',
+    components:{
+      HomeHeader
+    }
   }
 </script>
 
