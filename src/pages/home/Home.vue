38db80226bda65422b50c499c9f1be48c8ddc06a
xmm <1195176384@qq.com>
2020-08-14 18:08:50 +0800

    finish pages

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -16,6 +16,7 @@ import HomeIcons from'./components/Icons'
 import HomeRecommend from'./components/Recommend'
 import HomeWeekend from'./components/Weekend'
 import axios from 'axios'
+import {mapState} from 'vuex'
 export default{
     name:'Home',
     components:{
@@ -27,15 +28,18 @@ export default{
     },
     data (){
       return{
+        lastCity:'',
         swiperList:[],
         iconList:[],
         recommendList:[],
         weekendList:[]
       }
+    },computed:{
+      ...mapState(['city'])
     },
     methods:{
       getHomeInfo (){
-        axios.get('/api/index.json')
+        axios.get('/api/index.json?city='+this.city)
              .then(this.getHomeInfoSucc)
       },
       getHomeInfoSucc(res){
@@ -49,8 +53,15 @@ export default{
         }
       }
     },
-    mounted (){
+    mounted (){/* 页面挂载时执行 */
+      this.lastCity=this.city
       this.getHomeInfo()
+    },
+    activited(){/* 页面重新被显示时执行 */
+      if (this.lastCity!==this.city) {
+        this.lastCity=this.city
+        this.getHomeInfo()/* 重新发送ajax请求 */
+      }
     }
   }
 </script>
